1b41ff40e413e14eae68fdab3c2a813426ab4ecd
choikangcheol <skynlake@gmail.com>
2024-12-19 20:47:48 +0900

    feat: 메시지를 출력하고 콘솔에서 int 값을 읽어 들임

--- AbilityScore/Program.cs
+++ AbilityScore/Program.cs
@@ -11,5 +11,21 @@ namespace AbilityScore
         static void Main(string[] args)
         {
         }
+
+        public static int ReadInt(int lastUsedValue, string prompt)
+        {            
+            Console.Write($"{prompt}[{lastUsedValue}]: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int value))
+            {
+                Console.WriteLine($"    using value {value}");
+                return value;
+            }
+            else
+            {
+                Console.WriteLine($"    using default value {lastUsedValue}");
+                return lastUsedValue;
+            }
+        }
     }
 }

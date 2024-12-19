c3fc0d3ea8ddd82dc86401cfe145c73477d45221
choikangcheol <skynlake@gmail.com>
2024-12-19 13:02:20 +0900

    Add project files.

---
+++ AbilityScore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbilityScore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

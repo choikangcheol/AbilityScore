c4701b055320f92c3cfd6cc802f4eab7d4a8cf59
choikangcheol <skynlake@gmail.com>
2024-12-19 13:22:56 +0900

    Comments: 코드 설명 추가
    추가한 내용: 소수점 이하를 버리고 정수를 만듦

--- AbilityScore/AbilityScoreCalculator.cs
+++ AbilityScore/AbilityScoreCalculator.cs
@@ -19,7 +19,7 @@ namespace AbilityScore
             // 굴리기 값을 DivideBy 필드 값으로 나눕니다.
             double divided = RollResult / DivideBy;
 
-            // AddAmount를 나눗셈 결과에 더합니다.
+            // AddAmount를 나눗셈 결과에 더합니다. 소수점 이하를 버리고 정수를 만듦
             int added = AddAmount += (int)divided;
 
             // 결과 값이 너무 작으면 Minimum 값을 사용합니다.

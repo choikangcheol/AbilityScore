dc8dfdc61e427b08952771e78db092e929a3f636
choikangcheol <skynlake@gmail.com>
2024-12-19 21:14:21 +0900

    Feat: 능력치값을 계산하다록 Main 함수 작성

--- AbilityScore/Program.cs
+++ AbilityScore/Program.cs
@@ -10,6 +10,18 @@ namespace AbilityScore
     {
         static void Main(string[] args)
         {
+            AbilityScoreCalculator calculator = new AbilityScoreCalculator();
+            while (true)
+            {
+                calculator.RollResult = ReadInt(calculator.RollResult, "Starting 4d6 roll");
+                calculator.DivideBy = ReadDouble(calculator.DivideBy, "Divide by");
+                calculator.AddAmount = ReadInt(calculator.Minimum, "Minimum");
+                calculator.CalculateAbilityScore();
+                Console.WriteLine($"Calculated ability score: {calculator.Score}");
+                Console.WriteLine("Press Q to quit, and other key to continue.");
+                char keyChar = Console.ReadKey(true).KeyChar;
+                if (keyChar == 'Q' || keyChar == 'q') return;
+            }
         }
 
         public static int ReadInt(int lastUsedValue, string prompt)
